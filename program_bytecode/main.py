89af9698c9b33d2b95d2d6ea916bbc7943e6b311
AZZAMAF <aafazzam@gmail.com>
2025-09-02 18:50:28 +0700

    Initial commit: add main.py for bytecode test

---
+++ program_bytecode/main.py
@@ -0,0 +1,17 @@
+import time
+start_time = time.time()
+for i in range(1,1000):
+    a = 10
+print(time.time() - start_time, "detik")
+
+
+#kita bisa mengcompile python ke namanya bytecode
+#bytecode = bahasa penengah antara python sama mesin
+#compli = penerjemah ke bahasa komputer
+# jadi sesuatu yang sudah di compile atau bahasa programan compiler 
+# akan lebih cepat karena langsung menggukana bahasa komputer
+
+#make compailer = python3 -m py_compile main.py
+# TATA CARA RUN
+#cd __pycache__ -> python3 main tab/ enter/ jalan deh
+#cd.. = turun satu folder
