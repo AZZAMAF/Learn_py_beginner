ac29a11c717bf31b4998a2809638785f711e5533
AZZAMAF <aafazzam@gmail.com>
2025-09-05 17:54:46 +0700

    Logic and Bolean

---
+++ Logic and Boolean/Main.py
@@ -0,0 +1,41 @@
+#Oprasi Logika and bolean
+# NOT, OR, AND, NOT
+
+#NOT
+
+print("=== NOT ===")
+a = True
+c = not a
+print('data bolean a =', a)
+print('--------- Not')
+print('data bolean c =', c)   
+
+
+#OR // if one of them tru = is true 
+
+print("\n=== OR ===")
+a = False
+b = True
+c = a or b
+print(a,'OR',b,'=',c)
+
+
+#AND // 22 nya harus True
+
+print("\n=== AND ===")
+a = True
+b = True
+c = a and b
+print(a,'AND',b,'=',c)
+
+
+#XOR // Dia akan Tru jika salah satu TRUE sisanya False
+print("\n=== XOR ===")
+a = True
+b = False
+c = a ^ b
+print(a,'XOR',b,'=',c)
+
+
+
+
