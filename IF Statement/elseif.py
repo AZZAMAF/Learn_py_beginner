30fdc614701db8c91f24f5d93ea7ca906b499751
AZZAMAF <aafazzam@gmail.com>
2025-09-11 19:54:14 +0700

    If and elif statement

---
+++ IF Statement/elseif.py
@@ -0,0 +1,14 @@
+# Elif = else if statement
+
+nama = input("Nama Kamyu siapa?")
+if nama == "ucup":
+    print('Hai ganteng beuds!')
+elif nama == "otong":
+    print('hai tong pie kabere?')
+elif nama:
+    print(f"hai {nama} kamu kece bet dah")
+else:
+    print("sok asik jing")
+    
+print("ini adalah akhir dari program")
+
---
+++ IF Statement/ifelse.py
@@ -0,0 +1,35 @@
+#if and else statement
+
+# 1. if nya
+# 2. kondisinya
+# 3. Aksinya
+
+nama = input("Your Name:")
+
+
+#if kondisi: aksi
+#program Selanjutnya
+
+# 1. program if inline
+#if nama == "ucup" : print("Kamu ganteng")
+#print(f"Thank YOu {nama}")
+
+# 2. program if indentation
+if nama == "ucup" : 
+    print('Kamu ganteng')
+    print("sok mantep lu")
+print(f"Thank YOu {nama}")
+
+# 3. Else Statment
+if nama == "otong":
+    print('Hai otong surotong')
+else:
+    print('siapa lu gaje?')
+print('sokbet mantep')
+
+if nama :
+    print('salam kenal')
+
+
+
+    
