c650adbf62ecb5610064c0ade09621ba9376a263
AZZAMAF <aafazzam@gmail.com>
2025-09-19 10:00:59 +0700

    Init learn

---
+++ init/main.py
@@ -0,0 +1,7 @@
+import sains
+
+hasilTambah = sains.matematika.tambah(23,23)
+print(f'Hasil tambah : {hasilTambah}')
+
+hasilFisika = sains.fisika.gaya(12,12)
+print('this output from Fisika',hasilFisika)
