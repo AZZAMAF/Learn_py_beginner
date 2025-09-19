414b36be0b5b4463ae351b70194f5a2ef9b85d91
AZZAMAF <aafazzam@gmail.com>
2025-09-19 08:43:24 +0700

    Import statment

---
+++ import Statment/main.py
@@ -0,0 +1,17 @@
+# import
+# fungsi adalah untuk mengambil program dari file external .py
+
+# 1. untuk nyeambung prgram
+import call
+
+#2. import with data
+import variabel
+
+# data in name space (Variabel)
+print(variabel.data)
+
+#3. import with function
+import mtk
+
+hasil = mtk.tambah(4,5)
+print(hasil)
---
+++ import Statment/variabel.py
@@ -0,0 +1,1 @@
+data = 'azzam shiball seikaayaa'
